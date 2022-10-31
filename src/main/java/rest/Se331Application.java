9db5298520c7d20265d22b0e8fda8efb24363f46
supachok jrirarojkul <supachok8728@gmail.com>
2022-11-01 02:52:34 +0700

    deploy backend

--- src/main/java/rest/Se331Application.java
+++ src/main/java/rest/Se331Application.java
@@ -17,7 +17,7 @@ public class Se331Application {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000", "http://44.210.136.198:8001")
+                        .allowedOrigins("http://localhost:3000", "http://3.221.234.44:3000")
                         .exposedHeaders("x-total-count");
 
             }
